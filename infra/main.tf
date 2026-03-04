16ce69268dc45e2a54db1957a6936dc7daa8e2c9
Lucas <lucascsvet@gmail.com>
2026-03-04 20:12:33 -0300

    #0007 - Liberando porta Prometheus

--- infra/main.tf
+++ infra/main.tf
@@ -18,6 +18,14 @@ resource "aws_security_group" "imc_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
